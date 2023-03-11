26faab7e535cdec130b8c3cb586d8775191f8970
Sajeel-Haider <90964891+Sajeel-Haider@users.noreply.github.com>
2023-03-11 23:03:32 +0500

    added some animations

---
+++ JavaScript/mainPage.js
@@ -0,0 +1,26 @@
+const punchLine = document.querySelector(".punchLine h1");
+const pLText = punchLine.textContent;
+console.log(pLText);
+
+punchLine.textContent = "";
+
+const splitpL = pLText.split("");
+console.log(splitpL);
+for (let i = 0; i < splitpL.length; i++) {
+  punchLine.innerHTML += "<span>" + splitpL[i] + "</span>";
+}
+let char = 0;
+let timer = setInterval(onTick, 50);
+function onTick() {
+  const span = punchLine.querySelectorAll("span")[char];
+  span.classList.add("fade");
+  char++;
+  if (char === pLText.length) {
+    complete();
+    return;
+  }
+}
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
