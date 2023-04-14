dea13430ddfe8592603b86dc506c107c146959e1
Sajeel-Haider <90964891+Sajeel-Haider@users.noreply.github.com>
2023-04-14 22:18:40 +0500

    ok

--- JavaScript/mainPage.js
+++ JavaScript/mainPage.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const punchLine = document.querySelector(".punchLine h1");
+const btnScrollTo = document.querySelector(".btn--scroll");
+const sectionExpl = document.querySelector("#section--exploreId");
+const exploreBtn = document.querySelector(".nav--explore");
 
 const pLText = punchLine.textContent;
 console.log(pLText);
@@ -27,3 +30,31 @@ function complete() {
   clearInterval(timer);
   timer = null;
 }
+
+// Button scrolling
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = sectionExpl.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    "height/width viewpoTTrt",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  sectionExpl.scrollIntoView({ behavior: "smooth" });
+});
+
+exploreBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav--explore")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
