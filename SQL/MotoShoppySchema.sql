e946ed0beb542d493956bc29e910ebf79c20ba67
YVahedy <67382689+YVahedy@users.noreply.github.com>
2023-04-28 18:40:23 +0500

    Update MotoShoppySchema.sql

--- SQL/MotoShoppySchema.sql
+++ SQL/MotoShoppySchema.sql
@@ -27,12 +27,16 @@ create table products
 	foreign key (sid) references accounts (aid),
 	);
 
+alter table products add constraint delSellerProducts foreign key (sid) references accounts (aid) on delete cascade --to delete products of a seller when seller is removed
+
 create table orders (
   oid int primary key,
   cid INT,
   Date DATE,
   Delivered INT,
   foreign key (cid) references accounts(aid));
+
+alter table orders add constraint nullcusID foreign key (cid) references accounts (aid) on delete set null 
 
 create table order_detail (
 	oid int not null,
@@ -41,6 +45,9 @@ create table order_detail (
 	foreign key (oid) references orders (oid),
 	foreign key (pid) references products (pid),
 	primary key (oid,pid));
+
+alter table order_detail add constraint nullpID foreign key (pid) references products (pid) on delete cascade --because pid not nullable
+
 
 create table reviews (
 	oid int foreign key references orders (oid),
@@ -122,3 +129,133 @@ end
 declare @status int
 exec Login 'fahad.ajmal@gmail.com','fahad123',@status output
 select @status as Status
+
+--TOP SELLING
+CREATE VIEW topSelling
+AS
+SELECT Top 5 p.pid FROM order_detail o
+JOIN products p ON p.pid=o.pid
+GROUP BY p.pid
+ORDER BY  SUM(o.quantity) desc
+
+SELECT * FROM topSelling
+
+
+--SEARCH PRODUCTS
+Create Procedure searchProducts
+@name varchar(100)
+AS
+BEGIN
+	if EXISTS (SELECT * FROM products Where pName LIKE ('%'+@name+'%'))
+	BEGIN
+		SELECT * FROM products Where pName LIKE ('%'+@name+'%')
+	END
+	else
+	BEGIN
+		print 'Product not found'
+	END
+END
+EXEC searchProducts
+@name='Leather'
+
+--CATEGORY
+CREATE Procedure categorize
+@cate varchar(100)
+AS
+BEGIN
+	if EXISTS (SELECT * FROM products Where category=@cate)
+	BEGIN
+		SELECT * FROM products Where category=@cate
+	END
+	else
+	BEGIN
+		print 'No Such Category Exists'
+	END
+END
+EXEC categorize
+@cate='Safety Gear'
+
+--UPDATE ACCOUNT PROCEDURE
+create procedure UpdateAcc
+@aName varchar(20),@city varchar(15),@phone varchar(12),@email varchar(30),@pass varchar(20),@type int,@address varchar(100),@status int output
+as begin
+	declare @num int
+	select @num=count(*) from accounts a
+	where @email=a.email and @pass=a.password
+	if (@num=1) --
+	begin
+		declare @aid int
+		select @aid=(max(a.aid)+1) from accounts a
+		if (@aid is NULL)
+		begin
+			set @aid=1
+		end
+		insert into accounts values
+		(@aid,@aName,@city,@phone,@email,@pass,@type,@address);
+		set @status=1
+		Print 'Signup Successful'
+	end
+	else
+	begin
+		set @status=0
+		Print 'Signup Unsuccessful, email already exists'
+	end
+end
+--TESTING SIGNUP
+declare @status int
+exec SignUp'Fahad Ajmal','Lahore','03090078602','fahad.ajmal@gmail.com','fahad123',2,'House #4, Street #20,Block B, Phase 2, PCSIR, Lahore',@status output
+select @status as Status
+
+--DELETE PRODUCT PROCEDURE
+create procedure delProduct
+@pid int,@status int output
+as begin
+	declare @num int
+	select @num = count(*) from products where pid=@pid
+	if (@num!=0)
+	begin
+		set @status=1
+		delete from products
+		where pid=@pid
+		Print 'Product deleted'
+	end
+	else
+	begin
+		set @status=0
+		Print 'No product found'
+	end
+end
+--DELETE PRODUCT TESTING
+insert into products values (1, 'Helmet', 'Safety Gear','Scorpion Helmet, 2 DOT Rating',3500,5,1,NULL)
+select * from products
+declare @status int
+exec delProduct 1,@status output
+
+--DELETE ACCOUNT PROCEDURE
+create procedure delAccount
+@aid int,@status int output
+as begin
+	declare @num int
+	select @num = count(*) from accounts where aid=@aid
+	if (@num!=0)
+	begin
+		set @status=1
+		delete from accounts
+		where aid=@aid
+		Print 'Account deleted'
+	end
+	else
+	begin
+		set @status=0
+		Print 'No Account found'
+	end
+end
+--DELETE ACCOUNT TESTING
+select * from accounts
+declare @status int
+exec delAccount 2,@status output
+declare @status int
+exec SignUp'Fahad Ajmal','Lahore','03090078602','fahad.ajmal@gmail.com','fahad123',2,'House #4, Street #20,Block B, Phase 2, PCSIR, Lahore',@status output
+
+
+
